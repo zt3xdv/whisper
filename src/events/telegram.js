5bf8069d48063acce5cd834d7eeb9ffe854bdbbe
zt3xdv <zstr3mx.dev@gmail.com>
2026-08-08 14:05:06 -0300

    Fixed telegram forwarding

--- src/events/telegram.js
+++ src/events/telegram.js
@@ -1,29 +1,36 @@
 import { Events } from "discord.js";
-module.exports = {
-    name: Events.MessageCreate,
-    async execute(message) => {
-        const whitelist_channels = [ 
-            '1146395460710436945', // #updates
-            '1269151398105448458' // #announcements
-        ];
-        
-        if (!whitelist_channels.includes(message.channelId)) return;
-        
-        const url = `https://api.telegram.org/bot8679922182:AAFfK1DlkKaH2SIoutAJdTZNiPmtkFc8pvI/sendMessage`;
-        try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                chat_id: '-1003747712686', // @wispbyte
-                text: message.cleanContent
-            })
-        });
+import config from "../../config.json" with { type: "json" };
 
-        const data = await response.json();
-        console.log('Success sending telegram message:', data);
+const whitelistChannels = [ 
+  '1146395460710436945', // #updates
+  '1269151398105448458' // #announcements
+];
+
+export default {
+  name: Events.MessageCreate,
+  async execute(message) => {
+    if (!whitelistChannels.includes(message.channelId)) return;
+    
+    try {
+      const response = await fetch(`https://api.telegram.org/bot${config.telegramId}/sendMessage`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          chat_id: config.telegramChatId,
+          text: message.cleanContent
+        })
+      });
+      
+      if (!response.ok) {
+        const err = await response.text();
+        throw new Error(err);
+      }
+      
+      // Since it will spam console i guess
+      // const data = await response.json();
+      // console.log('Success sending telegram message:', data);
     } catch (error) {
-        console.error('Error sending telegram message:', error);
+      console.error('Error sending telegram message:', error);
     }
-    }
+  }
 };
