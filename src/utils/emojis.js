faa8b75032ffd7bca622e2e8bafef1e8bb325229
zt3xdv <zstr3mx.dev@gmail.com>
2026-08-12 15:53:20 -0300

    Fixed typo

--- src/utils/emojis.js
+++ src/utils/emojis.js
@@ -2,7 +2,7 @@ export const emojis = {
   "right": "<:right:1535650349606764634>",
   "left": "<:left:1535650428468068372>",
   "settings": "<:settings:1535651301822570556>",
-  "exclamation": "<:exclamation:1535654003768164414>"?
+  "exclamation": "<:exclamation:1535654003768164414>",
   "wrong": "<:wrong:1535654508913500180>",
   "correct": "<:correct:1535654631105896448>",
   "pings": "<:pings:1535656498280013884>"
