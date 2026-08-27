0fd79e3c5f659aef4d6ed16152077315e362b5cc
alexutzuu <thebrainsurgeon4@gmail.com>
2026-08-27 18:56:53 +0300

    Add comment 'very useful line' to ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,6 @@ import { Events } from "discord.js";
 export default {
   name: Events.ClientReady,
   async execute(client) {
-    console.log("Logged in as", client.user.tag);
+    console.log("Logged in as", client.user.tag); // very useful line
   }
 };
