295592a9b69972b4205668f5dbe31fb4b3ee5b4f
zt3xdv <zstr3mx.dev@gmail.com>
2026-08-27 16:22:04 -0300

    Fix imports

--- src/commands/aiProvider.js
+++ src/commands/aiProvider.js
@@ -1,7 +1,7 @@
 import { MessageFlags, ComponentType, ApplicationCommandOptionType } from "discord.js";
 import { emojis } from "../utils/emojis.js";
 import { isStaff } from "../utils/staff.js";
-import { resetEphemeralAiProvider, setEphemeralAiProvider } from '../events/messageCreate.js';
+import * as MessageCreate from '../events/messageCreate.js';
 
 export default {
   name: "aiprovider",
@@ -61,12 +61,12 @@ export default {
       const auth = interaction.options.getString('authorization') ?? null;
       const maxTokens = interaction.options.getInteger('maxtokens');
 
-      setEphemeralAiProvider(url, auth, model, maxTokens);
+      MessageCreate.setEphemeralAiProvider(url, auth, model, maxTokens);
 
       interaction.reply({ content: 'Changed AI provider. This will reset on restart or failed response', flags: 'Ephemeral' });
     }
     else if (subcommand === 'reset') {
-      resetEphemeralAiProvider();
+      MessageCreate.resetEphemeralAiProvider();
       interaction.reply({ content: 'Reset AI provider to default', flags: 'Ephemeral' });
     }
     else {
