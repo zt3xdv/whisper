6f0193348056e7de3245afb07f7beedfe13d0e67
zt3xdv <zstr3mx.dev@gmail.com>
2026-08-30 10:35:24 -0300

    Added a check when a user unboosts

---
+++ src/events/guildMemberUpdate.js
@@ -0,0 +1,19 @@
+import { Events } from "discord.js";
+import { isStaff } from "../utils/staff.js";
+
+export default {
+  name: Events.GuildMemberUpdate,
+  async execute(oldMember, newMember) {
+    if (oldMember.premiumSince && !newMember.premiumSince && !isStaff(newMember)) {
+      const { guild, client, user } = newMember;
+      const key = `br_${guild.id}_${user.id}`;
+      const roleId = await client.db.get(key);
+
+      if (roleId) {
+        const role = await guild.roles.fetch(roleId).catch(() => null);
+        if (role) await role.delete().catch(() => {});
+        await client.db.delete(key);
+      }
+    }
+  }
+};
