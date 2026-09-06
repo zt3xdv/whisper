d500889571004efd83c53314911fb31b2cbd9e89
zt3xdv <zstr3mx.dev@gmail.com>
2026-09-06 15:52:08 -0300

    Fixed client usage

--- src/commands/say.js
+++ src/commands/say.js
@@ -18,7 +18,7 @@ export default {
   async execute(interaction) {
     const text = interaction.options.getString("text", true);
     await interaction.channel.send({ content: text });
-    await addAuditLog(client.db, interaction.guildId, interaction.author.toString(), `Used /say command with content: ${text}`);
+    await addAuditLog(interaction.client.db, interaction.guildId, interaction.author.toString(), `Used /say command with content: ${text}`);
     
     return await interaction.reply({
       components: [
